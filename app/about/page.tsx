4469a3d27486c6bdb253451652c3519bb8a5896d
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-10-27 22:14:42 +0100

    Add content to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,43 @@
+import "./about.css";
+
 const AboutPage = () => {
-  return <main>AboutPage</main>;
+  return (
+    <div className="about-page">
+      <section className="about-page-section">
+        <h2>About LimitedEdition Bricks Hub</h2>
+        <p>
+          Welcome to LimitedEdition Bricks Hub, your premier destination for the
+          most exclusive and sought-after LEGO collectibles. We're passionate
+          about bringing the rarest and most limited-edition LEGO sets right to
+          your fingertips.
+        </p>
+      </section>
+      <section className="about-page-section">
+        <h2>Preorder Your LEGO Dreams</h2>
+        <p>
+          At LimitedEdition Bricks Hub, we believe in celebrating the essence of
+          LEGO's artistry and innovation. That's why we offer you the
+          opportunity to preorder the most coveted LEGO collectibles before they
+          hit the shelves. By preordering, you secure your piece of LEGO history
+          and become a part of a global community of collectors who cherish the
+          extraordinary.
+        </p>
+        <p>
+          Our curated selection includes limited-edition sets that embody the
+          spirit of creativity, imagination, and the LEGO legacy. Each set tells
+          a unique story, and we're here to ensure you're part of that
+          narrative.
+        </p>
+        <p>
+          Explore our catalog, embark on your LEGO journey, and preorder your
+          next masterpiece today. Thank you for choosing LimitedEdition Bricks
+          Hub to be a part of your LEGO adventure.
+        </p>
+
+        <button className="about-pre-order-button">Pre-order now</button>
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;
