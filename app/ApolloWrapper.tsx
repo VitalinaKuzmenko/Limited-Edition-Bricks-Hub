a6ec983964946ea27449346d420a035d14c3c5c5
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-10-30 11:41:10 +0000

    Update link for server on render.com

--- app/ApolloWrapper.tsx
+++ app/ApolloWrapper.tsx
@@ -13,7 +13,8 @@ import {
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "http://localhost:4000",
+    // uri: "http://localhost:4000",
+    uri: "https://limited-edition-bricks-hub-server.onrender.com/",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
