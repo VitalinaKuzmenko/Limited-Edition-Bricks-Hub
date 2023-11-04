975a7058c5f9fdceb9c6bbd162bdb7718df4b82e
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-04 13:53:17 +0000

    Create circle for heart in shop item

--- app/components/FeaturedItems/components/ShopItem.tsx
+++ app/components/FeaturedItems/components/ShopItem.tsx
@@ -63,11 +63,15 @@ const ShopItem: React.FC<ShopItemProps> = ({ shopItem }) => {
           />
         )}
 
-        <img
-          className="like-icon"
-          src="/icons/empty-heart-icon.svg"
-          alt="empty heart"
-        />
+        <div className="like-icon-container">
+          <Image
+            className="like-icon"
+            src="/icons/empty-heart-icon.svg"
+            width={30}
+            height={30}
+            alt="empty heart"
+          />
+        </div>
       </div>
       <div className="shop-items-details">
         <h2 className="name">{name}</h2>
