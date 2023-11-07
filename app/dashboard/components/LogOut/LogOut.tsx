f220262bd1b3094dc1b0bf17757690117b1e99f5
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-11-07 19:11:56 +0000

    Handle log out of user

--- app/dashboard/components/LogOut/LogOut.tsx
+++ app/dashboard/components/LogOut/LogOut.tsx
@@ -1,13 +1,29 @@
 import "./LogOut.css";
+import { signOut } from "firebase/auth";
+import { auth } from "@/firebaseConfig";
+import { useRecoilState } from "recoil";
+import { isUserLoginState } from "@/app/recoil/atoms";
+import { useRouter } from "next/navigation";
 
 const LogOut = () => {
+  const [_, setIsUserLogin] = useRecoilState(isUserLoginState);
+  const router = useRouter();
+
+  const handleLogOut = () => {
+    signOut(auth);
+    setIsUserLogin(false);
+    router.push("/");
+  };
+
   return (
     <div className="logout-container">
       <h1>
         Are you sure you want
         <br /> to log out?
       </h1>
-      <button className="logout-button">Yes</button>
+      <button className="logout-button" onClick={handleLogOut}>
+        Yes
+      </button>
     </div>
   );
 };
